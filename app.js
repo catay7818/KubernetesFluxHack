e25a6532d30f1553596566f8ccd25048dcaff59a
Cameron Taylor <cameron.taylor@microsoft.com>
2024-05-20 12:57:50 -0700

    new message for demo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Flux!'))
+app.get('/', (req, res) => res.send('Hello Flux! This is a demo.'))
 app.listen(3000, () => console.log('Server ready'))
